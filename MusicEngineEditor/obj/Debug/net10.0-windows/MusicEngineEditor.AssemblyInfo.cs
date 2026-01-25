45df5249748368dcbf5b50ce61edd471ab5b04fd
nullonebinary <bytezer0ffensive@protonmail.com>
2026-01-25 15:44:39 +0100

    New Synthesizers:   - SupersawSynth: JP-8000 style with 1-16 unison oscillators, stereo spread   - AdditiveSynth: Harmonic series synthesis with Hammond-style drawbars
      New Effects:
      - SubBassGenerator: Generates sub-harmonics (Octave, Fifth, DualOctave)
      - HarmonicEnhancer: Even/odd harmonics, tube/tape saturation, air
      - Dither: RPDF/TPDF/HP-TPDF with noise shaping (ModifiedE, FWeighted, ImprovedE)
    
      New Sequencing:
      - StepSequencer: Drum machine style, multi-row, direction modes
      - ProbabilitySequencer: Per-step probability, ratchet, conditions
      - Humanizer: Timing/velocity randomization, preserve accents

--- MusicEngineEditor/obj/Debug/net10.0-windows/MusicEngineEditor.AssemblyInfo.cs
+++ MusicEngineEditor/obj/Debug/net10.0-windows/MusicEngineEditor.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2025 MusicEngine")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Professional music production and audio scripting environment")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b2e5249a00eaa4c73eb57739016edf378d2f41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8a35b3543c739624c12fd5f9498c84aa70a2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngineEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
